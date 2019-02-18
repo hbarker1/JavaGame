086df5777a1af79c3af6aad609bb50ce90c37091
hannah barker <hannah.m.barker@gmail.com>
2019-02-17 19:13:11 -0500

    first commit

---
+++ java-game-01.js
@@ -0,0 +1,56 @@
+
+
+// let game = prompt ('Do you want to play a game?')
+// console.log(game);
+
+// if (game === 'yes') {
+//     let name = prompt ('What is your name?')
+//     console.log(name);
+// }
+
+
+// else {
+// }
+
+
+let grant = 'Grant the Mighty Chicken';
+let user = name;
+let userHealth = 40;
+let grantHealth = 10;
+let attack1 = Math.floor((Math.random()*3));
+let attack2 = Math.floor((Math.random()*3));
+
+
+
+
+
+do {
+    console.log(`${grant} has ${grantHealth - attack1} health left`);
+
+    console.log(`${user} has ${userHealth - attack2} health left`);
+    break;
+
+}while (grantHealth <= 10 && userHealth <= 40)
+
+    
+
+
+//     if (grantHealth === 0) {
+//         console.log(`${grant} has been defeated`);
+        
+       
+        
+    
+//     } 
+
+//     else if (userHealth === 0) {
+//         console.log(`${user} has been defeated`);
+//         clearInterval(userHealth);
+//             let name2 = prompt (`${user} want to try again?`);
+//             console.log (name2);
+//         }
+
+       
+
+  
+
